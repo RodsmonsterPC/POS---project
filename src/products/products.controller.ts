11bb35885d82725377a72e4c9a36476a1d205d57
RodsmonsterPC <rodofochido@gmail.com>
2025-03-26 16:48:40 -0600

    Finalización producto

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -30,12 +30,12 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(@Param('id', IdValidationPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', IdValidationPipe) id: string) {
     return this.productsService.remove(+id);
   }
 }
