c5735612d51a542ae20a0eb965d68443c5ef8888
RodsmonsterPC <rodofochido@gmail.com>
2025-03-26 11:25:52 -0600

    Avance en productos

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,21 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+
+    @IsNotEmpty({message: 'Nombre del producto es obligatorio'})
+    @IsString({message:'Valor no válido'})
+    name: string
+
+    @IsNotEmpty({message: 'El valor es obligatorio'})
+    @IsNumber({maxDecimalPlaces: 2}, {message: 'Precio no válido'})
+    price: number
+
+    @IsNotEmpty({message: 'Cantidad no puede ir vacia'})
+    @IsNumber({maxDecimalPlaces: 0}, {message: 'Cantidad no válida'})
+    inventory: number
+
+    @IsNotEmpty({message: 'La categoría es obligatoria'})
+    @IsInt({message: 'La categoría no es válida'})
+    categoryId: number
+
+}
