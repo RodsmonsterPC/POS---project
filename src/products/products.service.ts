02a8018eaa0bedbc1ce258cb08462365e7590cf3
RodsmonsterPC <rodofochido@gmail.com>
2025-04-08 12:13:50 -0600

    Ajustes eliminar producto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -109,6 +109,6 @@ export class ProductsService {
     const product = await this.findOne(id)
     await this.productRepository.remove(product)
 
-    return `Producto Eliminado`;
+    return {message: 'Producto Eliminado'};
   }
 }
